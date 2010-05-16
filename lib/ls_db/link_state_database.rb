b93783ad321da427da0c33e52b71418ce6fbcb26
Jean-Michel Esnault <jesnault@gmail.com>
2010-05-16 08:33:08 -0700

    adding copyright

--- lib/ls_db/link_state_database.rb
+++ lib/ls_db/link_state_database.rb
@@ -1,7 +1,26 @@
+#--
+# Copyright 2010 Jean-Michel Esnault.
+# All rights reserved.
+# See LICENSE.txt for permissions.
 #
-#  Created by Jean-Michel Esnault.
-#  Copyright (c) 2008. All rights reserved.
 #
+# This file is part of OSPFv2.
+# 
+# OSPFv2 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# OSPFv2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OSPFv2.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
+
 
 =begin rdoc
 
--- lib/neighbor_sm/exstart_state.rb
+++ lib/neighbor_sm/exstart_state.rb
@@ -1,3 +1,25 @@
+#--
+# Copyright 2010 Jean-Michel Esnault.
+# All rights reserved.
+# See LICENSE.txt for permissions.
+#
+#
+# This file is part of OSPFv2.
+# 
+# OSPFv2 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# OSPFv2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OSPFv2.  If not, see <http://www.gnu.org/licenses/>.
+#++
+
 require 'neighbor_sm/neighbor_state'
 module OSPFv2
   module NeighborState
